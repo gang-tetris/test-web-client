522d7cef0ef13e8fe05cd7168d74f628905027a2
char-lie <5962274+char-lie@users.noreply.github.com>
2016-05-17 22:04:44 +0300

    Added controls for destination PORT

--- html/PersonManager.js
+++ html/PersonManager.js
@@ -2,9 +2,15 @@
 
 class PersonManager {
 
-    constructor(statusBar, panel) {
+    constructor(statusBar, panel, portField) {
         this._statusBar = statusBar;
         this._panel = panel;
+        this._portField = portField;
+        this.checkPorts();
+    }
+
+    setPort(port) {
+        this._port = port;
     }
 
     setStatus(description, error, code) {
@@ -22,29 +28,29 @@ class PersonManager {
 
     loadPerson(name) {
         this.setStatus(`Loading ${name}`);
-        var data_file = `http://localhost:8080/${name}`;
+        var data_file = `http://localhost:${this._port}/${name}`;
         var http_request = new XMLHttpRequest();
-        http_request = new XMLHttpRequest();
 
-        http_request.onreadystatechange = this.gotPerson.bind(this,
-                                                              http_request);
-        http_request.open("GET", data_file, true);
+        http_request.onreadystatechange = this.gotPerson.bind(
+                                                         this, http_request);
+        http_request.open('GET', data_file, true);
+
         http_request.send();
     }
 
     savePerson(name, age) {
-        var data_file = `http://localhost:8080/`;
+        var data_file = `http://localhost:${this._port}/`;
         var http_request = new XMLHttpRequest();
-        http_request = new XMLHttpRequest();
 
-        http_request.onreadystatechange = this.sentPerson.bind(this,
-                                                               http_request);
+        http_request.onreadystatechange = this.sentPerson.bind(
+                                                          this, http_request);
         http_request.open("POST", data_file, true);
         http_request.setRequestHeader('Content-Type', 'application/json');
-        http_request.send(JSON.stringify({
+        var data = {
             name: name,
             age: age
-        }));
+        };
+        http_request.send(JSON.stringify(data));
     }
 
     gotPerson(http_request) {
@@ -77,10 +83,12 @@ class PersonManager {
                 console.log(`Response is ${http_request.responseText}`);
                 var jsonObj = JSON.parse(http_request.responseText);
                 if (jsonObj.success) {
-                    this.displayPerson(jsonObj.response.person, false, http_request.status);
+                    this.displayPerson(jsonObj.response.person,
+                                       false, http_request.status);
                     this.displayServicesInfo(jsonObj.response);
                 } else {
-                    this.setStatus(jsonObj.error || "Error occured", true, http_request.status);
+                    this.setStatus(jsonObj.error || "Error occured",
+                                   true, http_request.status);
                     this.displayServicesInfo();
                 }
             } catch (e) {
@@ -109,6 +117,49 @@ class PersonManager {
                     response[key];
             }
         });
+    }
+
+    checkPorts() {
+        var port = 8080;
+        this.checkPort(port, [], (err, ports) => {
+            this.setPorts(ports);
+        });
+    }
+
+    setPorts(ports) {
+        console.log(`Set ports ${ports}`);
+        let port = this._port;
+        while (this._portField.firstChild) {
+            this._portField.removeChild(this._portField.firstChild);
+        }
+        ports.forEach((i) => {
+            let opt = window.document.createElement("option");
+            opt.value = i;
+            opt.textContent = i;
+            this._portField.appendChild(opt);
+        });
+        !port && (this._port = port = ports[0]);
+        this._portField.value = port;
+        setTimeout(this.checkPorts.bind(this), 10000);
+    }
+
+    checkPort(port, ports, callback) {
+        var data_file = `http://localhost:${port}`;
+        var http_request = new XMLHttpRequest();
+
+        http_request.onreadystatechange = () => {
+            if (http_request.readyState == 4) {
+                if (http_request.status == 200) {
+                    ports.push(port);
+                }
+                if (port > 8089) {
+                    return callback(null, ports);
+                }
+                this.checkPort(port + 1, ports, callback);
+            }
+        }
+        http_request.open("OPTIONS", data_file, true);
+        http_request.send();
     }
 }
 
--- html/index.js
+++ html/index.js
@@ -3,14 +3,20 @@
 window.onload = () => {
     var statusBar = window.document.getElementById('status');
     var panel = window.document.getElementById('manager-panel');
-    var personManager = new PersonManager(statusBar, panel);
+    var portField = window.document.getElementById('port');
+
+    var personManager = new PersonManager(statusBar, panel, portField);
     var personNameField = window.document.getElementById('name');
     var personAgeField = window.document.getElementById('age');
+
     window.document.getElementById('loader').onclick = () => {
         personManager.loadPerson(personNameField.value);
     }
     window.document.getElementById('saver').onclick = () => {
         personManager.savePerson(personNameField.value, personAgeField.value);
+    }
+    portField.onchange = () => {
+        personManager.setPort(portField.value);
     }
 }
 
